0f8f6e2f76febb09f4dbb41239ef7e0006a6b529
Jorge Betancourt <joisbetancourtes@ittepic.edu.mx>
2025-02-28 17:22:37 -0700

    Cambios con nota 7.1 funcional

--- webapp/controller/Main.controller.js
+++ webapp/controller/Main.controller.js
@@ -1,7 +1,7 @@
 sap.ui.define(["sap/ui/core/mvc/Controller"], (Controller) => {
-    "use strict";
-  
-    return Controller.extend("com.inv.sapfioriwebinvertions.controller.Main", {
-      onInit() {},
-    });
+  "use strict";
+
+  return Controller.extend("com.inv.sapfioriwebinvertions.controller.Main", {
+    onInit() {},
   });
+});
---
+++ webapp/controller/SalesForecast.controller.js
@@ -0,0 +1,16 @@
+sap.ui.define(
+  [
+    "com/inv/sapfioriwebinvertions/controller/BaseController",
+    "sap/ui/model/json/JSONModel",
+  ],
+  function (BaseController, JSONModel) {
+    "use strict";
+
+    return BaseController.extend(
+      "com.inv.sapfioriwebinvertions.controller.salesforecast.SalesForecast",
+      {
+        onInit: function () {},
+      }
+    );
+  }
+);
