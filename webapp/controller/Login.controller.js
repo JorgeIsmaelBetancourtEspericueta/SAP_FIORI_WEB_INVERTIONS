d63e85bdd6a21af6dc103ca45b780f9525ff1846
Jorge Betancourt <joisbetancourtes@ittepic.edu.mx>
2025-03-02 20:50:06 -0700

    Modifiqué el navbar

---
+++ webapp/controller/Login.controller.js
@@ -0,0 +1,8 @@
+sap.ui.define(["sap/ui/core/mvc/Controller"], (Controller) =>{
+    "use strict";
+
+    return Controller.extend("com.inv.sapfioriwebinvertions.controller.Login",
+        {
+            onInit() {},
+        });
+});
